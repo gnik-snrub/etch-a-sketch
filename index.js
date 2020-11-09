a3b3217339abcf621ecfe345ba319b472bf6d75d
gnik-snrub <60440393+gnik-snrub@users.noreply.github.com>
2020-11-10 04:08:16 +1100

    Added buttons to adjust grid width

--- index.js
+++ index.js
@@ -1,12 +1,13 @@
 const grid = document.querySelector('.grid');
 let gridSize = 600;
-let gridWidth = 11;
+let gridWidth = 16;
 
 let nodeSize = `${gridSize / gridWidth / 2 - 1}px`;
 
 createGrid();
 
 function createGrid() {
+    let nodeSize = `${gridSize / gridWidth / 2 - 1}px`;
     for (i = 0; i < gridWidth * gridWidth; i++) {
         const node = document.createElement('div');
         node.classList.add('node');
@@ -25,5 +26,20 @@ resetButton.addEventListener("click", reset);
 
 function reset() {
     const nodes = document.querySelectorAll('.node');
-    nodes.forEach(node => node.classList.remove('entered'));
+    nodes.forEach(node => node.remove());
+    createGrid();
+}
+
+const sizeChangers = document.querySelectorAll('.grid-change');
+const sizeDisplay = document.querySelector('.grid-size');
+sizeChangers.forEach(btn => btn.addEventListener('click', function() { changeSize(btn.value)}));
+
+
+function changeSize(val) {
+    gridWidth += parseInt(val);
+
+    if (gridWidth > 100) { gridWidth = 100; }
+    else if (gridWidth < 1) { gridWidth = 1; }
+
+    sizeDisplay.textContent = `Size: ${gridWidth}`;
 }
