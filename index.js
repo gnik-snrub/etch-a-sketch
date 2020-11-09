c293d12fd68365aeca320d0f5e2ecca0b44961ff
gnik-snrub <60440393+gnik-snrub@users.noreply.github.com>
2020-11-10 02:42:21 +1100

    Added reset button

--- index.js
+++ index.js
@@ -1,17 +1,29 @@
 const grid = document.querySelector('.grid');
 let gridSize = 600;
-let gridWidth = 50;
+let gridWidth = 11;
 
 let nodeSize = `${gridSize / gridWidth / 2 - 1}px`;
 
-for (i = 0; i < gridWidth * gridWidth; i++) {
-    const node = document.createElement('div');
-    node.classList.add('node');
-    node.style.padding = nodeSize;
+createGrid();
 
-    node.addEventListener('mouseover', function() {
-        node.classList.add('entered');
-    });
+function createGrid() {
+    for (i = 0; i < gridWidth * gridWidth; i++) {
+        const node = document.createElement('div');
+        node.classList.add('node');
+        node.style.padding = nodeSize;
+    
+        node.addEventListener('mouseover', function() {
+            node.classList.add('entered');
+        });
+    
+        grid.appendChild(node);
+    }
+}
 
-    grid.appendChild(node);
+const resetButton = document.querySelector('.reset');
+resetButton.addEventListener("click", reset);
+
+function reset() {
+    const nodes = document.querySelectorAll('.node');
+    nodes.forEach(node => node.classList.remove('entered'));
 }
